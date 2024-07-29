9a8631158593a45a14b809c6119be23948f5d7b0
mehdi <fetourimehdi0@gmail.com>
2024-07-29 17:23:20 +0200

    deleted:    ArgentBank-website-main/.eslintrc.cjs

--- ArgentBank-website-main/src/components/footer/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import "./Footer.scss";
-
-export default function Footer() {
-    return (
-        <footer>
-            <p className="footer-text">Copyright 2020 Argent Bank</p>
-        </footer>
-    );
-}
